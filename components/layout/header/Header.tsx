2f04305dd54a246864d21b04457f417c3425ea4e
iamjianyoudai <daijianyou@gmail.com>
2024-07-02 12:43:00 +0200

    // fix header load error // reset layout

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,5 +1,6 @@
+"use client";
 import useDarkMode from "@/components/useDarkMode";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CiDark, CiMenuBurger } from "react-icons/ci";
 import { MdOutlineLightMode } from "react-icons/md";
 import MobileNavigation from "./MobileNavigation";
@@ -9,6 +10,7 @@ export const Header = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   const [darkMode, setDarkMode] = useDarkMode();
   const [language, setLanguage] = useState("no");
+  const [mounted, setMounted] = useState(false);
 
   const flagIcons = {
     no: "🇳🇴",
@@ -16,22 +18,35 @@ export const Header = () => {
     zh: "🇨🇳",
   };
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // Render nothing on the server
+  }
+
   return (
     <header
       role="banner"
-      className="py-4 absolute w-full z-10"
+      className="lg:py-6 absolute w-full z-10"
       style={{ backgroundColor: "var(--color-bg-primary)" }}
     >
-      <div className="max-w-7xl mx-auto flex justify-between items-center px-4">
-        <div className="flex items-center">
-          <div className="text-black dark:text-white font-bold mr-8">Logo</div>
+      <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
+        {" "}
+        {/* Added padding here */}
+        <div className="flex items-center justify-start w-full">
+          {" "}
+          {/* Removed padding from here */}
           <Navigation language={language} setLanguage={setLanguage} />
         </div>
         <div className="flex items-center">
+          {" "}
+          {/* Separate container for language and dark mode controls */}
           <select
             value={language}
             onChange={(e) => setLanguage(e.target.value)}
-            className="appearance-none bg-[#f3f5f8] dark:bg-[#333333] dark:text-white text-black"
+            className="ml-4 bg-[#f3f5f8] dark:bg-[#333333] dark:text-white text-black lang-select"
           >
             <option value="no">{flagIcons.no} Norsk</option>
             <option value="en">{flagIcons.en} English</option>
@@ -42,9 +57,9 @@ export const Header = () => {
             className="text-black dark:text-white ml-4"
           >
             {darkMode ? (
-              <MdOutlineLightMode className="h-6 w-6" />
+              <MdOutlineLightMode className="h-5 w-5" />
             ) : (
-              <CiDark className="h-6 w-6" />
+              <CiDark className="h-5 w-5" />
             )}
           </button>
           <button
@@ -61,48 +76,3 @@ export const Header = () => {
 };
 
 export default Header;
-
-// import useDarkMode from "@/components/useDarkMode";
-// import { useState } from "react";
-// import { CiDark, CiMenuBurger } from "react-icons/ci";
-// import { MdOutlineLightMode } from "react-icons/md";
-// import MobileNavigation from "./MobileNavigation";
-// import Navigation from "./Navigation";
-
-// export const Header = () => {
-//   const [toggleMenu, setToggleMenu] = useState(false);
-//   const [darkMode, setDarkMode] = useDarkMode();
-//   const [language, setLanguage] = useState("no");
-
-//   return (
-//     <header
-//       role="banner"
-//       className="py-4 absolute w-full z-10"
-//       style={{ backgroundColor: "var(--color-bg-primary)" }}
-//     >
-//       <div className="max-w-7xl mx-auto flex justify-between items-center px-4">
-//         <div className="text-black dark:text-white font-bold">Logo</div>
-//         <Navigation language={language} setLanguage={setLanguage} />
-//         <button
-//           onClick={() => setDarkMode(!darkMode)}
-//           className="text-black dark:text-white ml-4"
-//         >
-//           {darkMode ? (
-//             <MdOutlineLightMode className="h-6 w-6" />
-//           ) : (
-//             <CiDark className="h-6 w-6" />
-//           )}
-//         </button>
-//         <button
-//           className="lg:hidden text-black dark:text-white font-semibold"
-//           onClick={() => setToggleMenu(!toggleMenu)}
-//         >
-//           <CiMenuBurger className="h-6 w-6" />
-//         </button>
-//       </div>
-//       <MobileNavigation isOpen={toggleMenu} language={language} />
-//     </header>
-//   );
-// };
-
-// export default Header;
